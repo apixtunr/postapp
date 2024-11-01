86dfd9304e04638341af240124dd01e4dfa4109e
Apix <147033703+apixtunr@users.noreply.github.com>
2024-11-01 15:34:39 -0600

    Update CorsConfig.java

--- backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/config/CorsConfig.java
+++ backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/config/CorsConfig.java
@@ -6,12 +6,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permitir todas las rutas
-                .allowedOrigins("http://149.50.145.214:5545") // Cambia esto por el origen que necesites
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://149.50.145.214:5545")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
+
