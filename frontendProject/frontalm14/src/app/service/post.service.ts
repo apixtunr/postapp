de96470bfd3d00a63b0b174dac33da83e52a317a
Apix <147033703+apixtunr@users.noreply.github.com>
2024-11-01 15:42:28 -0600

    Update post.service.ts

--- frontendProject/frontalm14/src/app/service/post.service.ts
+++ frontendProject/frontalm14/src/app/service/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -7,52 +7,29 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class PostService {
+
   private Url = environment.Url;
 
   constructor(private http: HttpClient) { }
   
-  // Método para crear un nuevo post
   createNewPost(post: any): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-
-    return this.http.post(`${this.Url}/api/posts`, post, {
-      headers: headers,
-      withCredentials: true
-    });
+    return this.http.post(this.Url + "/api/posts", post);
   }
 
-  // Método para obtener todos los posts
-  getAllPosts(): Observable<any> {
-    return this.http.get(`${this.Url}/api/posts`, {
-      withCredentials: true
-    });
+  getAllPosts(): Observable<any>{
+    return this.http.get(this.Url + "/api/posts");
   }
 
-  // Método para obtener un post por ID
-  getPostById(postId: number): Observable<any> {
-    return this.http.get(`${this.Url}/api/posts/${postId}`, {
-      withCredentials: true
-    });
+  getPostById(postId: number): Observable<any>{
+    return this.http.get(this.Url + "/api/posts/" + postId);
   }
 
-  // Método para dar like a un post
-  likePost(postId: number): Observable<any> {
-    return this.http.put(`${this.Url}/api/posts/${postId}/meGusta`, {}, {
-      withCredentials: true
-    });
+  likePost(postId: number): Observable<any>{
+    return this.http.put(this.Url + "/api/posts/" + postId+"/meGusta", {});
   }
 
-  // Método para buscar posts por nombre
-  searchByName(name: string): Observable<any> {
-    // Usar HttpParams para manejar parámetros de búsqueda
-    const params = new HttpParams().set('nombre', name);
-
-    return this.http.get(`${this.Url}/api/posts/buscar`, {
-      params: params,
-      withCredentials: true
-    });
+  searchByName(name: string): Observable<any>{
+    return this.http.get(this.Url + "/api/posts/buscar/" + name);
   }
+
 }
-
