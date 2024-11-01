34e0c0ff6366c4ee7fd956445c43d43ec30c91f1
Apix <147033703+apixtunr@users.noreply.github.com>
2024-11-01 12:02:30 -0600

    Update environment.prod.ts

--- frontendProject/frontalm14/src/environments/environment.prod.ts
+++ frontendProject/frontalm14/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    Url: 'http://149.50.145.214:9090' 
+    Url: 'http://springboot-app:9090' 
   };
   
