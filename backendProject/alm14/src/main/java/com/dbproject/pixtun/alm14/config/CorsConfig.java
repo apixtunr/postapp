79d1958c80954aaffc5455f2a62c8514860a8c4c
apixtunr <alvaro.pixtunramirez@gmail.com>
2024-11-01 20:45:03 -0300

    cors

---
+++ backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.dbproject.pixtun.alm14.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://vps-4501634-x.dattaweb.com:5545")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
