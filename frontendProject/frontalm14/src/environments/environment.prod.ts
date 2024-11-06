c433de7e48c3e236d32b563944e46effd384c1ef
Apix <147033703+apixtunr@users.noreply.github.com>
2024-11-05 22:53:20 -0600

    Update environment.prod.ts

--- frontendProject/frontalm14/src/environments/environment.prod.ts
+++ frontendProject/frontalm14/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-     Url: 'https://vps-4501634-x.dattaweb.com:5545'
+    Url: 'http://149.50.145.214:9090'
   };
   
