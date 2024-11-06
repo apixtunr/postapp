9a09db58db717a92120be43ba6f1118aa8ed59e6
Apix <147033703+apixtunr@users.noreply.github.com>
2024-11-06 11:18:56 -0600

    Update PostController.java

--- backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/controller/PostController.java
+++ backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/controller/PostController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
-@CrossOrigin(origins = "https://vps-4501634-x.dattaweb.com:5545")
+@CrossOrigin(origins = "http://vps-4501634-x.dattaweb.com:5545")
 @RequestMapping("api/posts")
 public class PostController {
     @Autowired
